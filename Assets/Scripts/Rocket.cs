c6581504c7ae48c4c1830016bb42a12a505925e5
Halim Lais <halimlais@hotmail.com>
2018-08-24 21:17:38 +0100

    levelsCOmplete

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,54 +1,111 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Rocket : MonoBehaviour {
+public class Rocket : MonoBehaviour
+{
+    //TODO Fix lighting bug
+   [SerializeField]
+    float rcsThrust = 100f;
 
-    
+    [SerializeField]
+    float mainRocketThrust = 100f;
+
     AudioSource rocketSound;
     Rigidbody rocketRigidBody;
-	// Use this for initialization
-	void Start () {
+
+    enum State {live,  trascending,dying };
+    State state = State.live;
+    // Use this for initialization
+    void Start()
+    {
 
         rocketRigidBody = GetComponent<Rigidbody>();
         rocketSound = GetComponent<AudioSource>();
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
+    }
 
-        ControllerInputs();
-		
-	}
-
-
-
-    void ControllerInputs()
+    // Update is called once per frame
+    void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        Thrusting();
+
+        Rotating();
+
+    }
+
+
+
+    void Rotating()
+    {
+
+        rocketRigidBody.freezeRotation = true;
+       
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
         {
-            rocketRigidBody.AddRelativeForce(Vector3.up);
+            
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+           
+            transform.Rotate(-Vector3.forward * rotationSpeed);
+        }
+
+        rocketRigidBody.freezeRotation = false; // resume rotation 
+
+    }
+
+    private void Thrusting()
+    {
+        float thrustSpeed = mainRocketThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rocketRigidBody.AddRelativeForce(new Vector3(0,1,0) * thrustSpeed);
             if (!rocketSound.isPlaying)
             {
                 rocketSound.Play();
             }
-            else rocketSound.Stop();
         }
-
-
-
-        if (Input.GetKey(KeyCode.A))
+        else
         {
-            transform.Rotate(Vector3.forward);
+
+            rocketSound.Stop();
         }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward);
-        }
-      
+
     }
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        switch (collision.gameObject.tag)
+        {
+            case "Finish":
+                state = State.trascending;
+               Invoke("LoadNextLevel",3f);
+               
+                break;
+
+            case "Friendly":
+                
+                break;
+            default:
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+                    
+                break;
+        }
+         //if (collision.gameObject.CompareTag("Friendly"))
+         //{
+         //    Debug.Log("hello");
+
+        //}
+
+    }
+    void  LoadNextLevel()
+    {
+       
+        int currentLevel = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentLevel + 1);
+    }
 }
